0b287fbd47a9443ba29f87c3ec19297a1e0d9f6c
Maria Serykh <marie.serykh@gmail.com>
2022-08-07 21:02:39 +0200

    refactor: refactor test of album controller

--- src/album/album.controller.spec.ts
+++ src/album/album.controller.spec.ts
@@ -7,34 +7,20 @@ describe('albumController', () => {
   let albumController: AlbumController;
 
   const mockAlbumService = {
-  };
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AlbumController],
-      providers: [AlbumService],
-    })
-      .overrideProvider(AlbumService)
-      .useValue(mockAlbumService)
-      .compile();
-
-    albumController = module.get<AlbumController>(AlbumController);
-  });
-
-  it('should be defined', () => {
-    expect(albumController).toBeDefined();
-  });
-
-});
-describe('Method create of the Album Controller', () => {
-  let albumController: AlbumController;
-
-  const mockAlbumService = {
     createAlbum: jest.fn(async (dto) => {
       return {
         id: uuidv4(),
         ...dto,
       };
+    }),
+    editAlbum: jest.fn(async (id: string, dto) => {
+      return {
+        id: id,
+        ...dto,
+      };
+    }),
+    deleteAlbum: jest.fn(async (id: string) => {
+      return null;
     }),
   };
 
@@ -50,61 +36,188 @@ describe('Method create of the Album Controller', () => {
     albumController = module.get<AlbumController>(AlbumController);
   });
 
-  it('should create a new album', async () => {
-    const dto = {
-      name: 'Hard Bop',
-      year: 1957,
-      artistId: null,
-    };
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-    const album = await albumController.createAlbum(dto);
+  it('should be defined', () => {
+    expect(albumController).toBeDefined();
+  });
 
-    expect(album).toEqual({
-      id: expect.any(String),
-      ...dto,
+  describe('Method create of the Album Controller', () => {
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        controllers: [AlbumController],
+        providers: [AlbumService],
+      })
+        .overrideProvider(AlbumService)
+        .useValue(mockAlbumService)
+        .compile();
+
+      albumController = module.get<AlbumController>(AlbumController);
+    });
+
+    it('should create a new album', async () => {
+      const dto = {
+        name: 'Hard Bop',
+        year: 1957,
+        artistId: null,
+      };
+
+      const album = await albumController.createAlbum(dto);
+
+      expect(album).toEqual({
+        id: expect.any(String),
+        ...dto,
+      });
+    });
+
+    it('should be called', async () => {
+      const dto = {
+        name: 'Hard Bop',
+        year: 1957,
+        artistId: null,
+      };
+
+      const album = await albumController.createAlbum(dto);
+
+      expect(mockAlbumService.createAlbum).toHaveBeenCalled();
+    });
+
+    it('should be called with the passed dto', async () => {
+      const dto = {
+        name: 'Hard Bop',
+        year: 1957,
+        artistId: null,
+      };
+
+      const album = await albumController.createAlbum(dto);
+
+      expect(mockAlbumService.createAlbum).toHaveBeenCalledWith(dto);
+
+      expect(mockAlbumService.createAlbum).toHaveBeenCalledWith({
+        name: 'Hard Bop',
+        year: 1957,
+        artistId: null,
+      });
+    });
+
+    it('should return an object with id', async () => {
+      const dto = {
+        name: 'Hard Bop',
+        year: 1957,
+        artistId: null,
+      };
+
+      const album = await albumController.createAlbum(dto);
+
+      expect(album).toHaveProperty('id');
+      expect(album).toHaveProperty('id', expect.any(String));
     });
   });
 
-  it('should be called', async () => {
-    const dto = {
-      name: 'Hard Bop',
-      year: 1957,
-      artistId: null,
-    };
+  describe('Method update of the Album Controller', () => {
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        controllers: [AlbumController],
+        providers: [AlbumService],
+      })
+        .overrideProvider(AlbumService)
+        .useValue(mockAlbumService)
+        .compile();
 
-    const album = await albumController.createAlbum(dto);
+      albumController = module.get<AlbumController>(AlbumController);
+    });
 
-    expect(mockAlbumService.createAlbum).toHaveBeenCalled();
-  });
+    it('should update an album', async () => {
+      const dto = {
+        name: 'Ritual',
+        year: 1957,
+        artistId: null,
+      };
 
-  it('should be called with the passed dto', async () => {
-    const dto = {
-      name: 'Hard Bop',
-      year: 1957,
-      artistId: null,
-    };
+      const album = await albumController.editAlbum(uuidv4(), dto);
 
-    const album = await albumController.createAlbum(dto);
+      expect(album).toEqual({
+        id: expect.any(String),
+        ...dto,
+      });
+    });
 
-    expect(mockAlbumService.createAlbum).toHaveBeenCalledWith(dto);
+    it('should be called', async () => {
+      const dto = {
+        name: 'Ritual',
+        year: 1957,
+        artistId: null,
+      };
 
-    expect(mockAlbumService.createAlbum).toHaveBeenCalledWith({
-      name: 'Hard Bop',
-      year: 1957,
-      artistId: null,
+      const album = await albumController.editAlbum(uuidv4(), dto);
+
+      expect(mockAlbumService.editAlbum).toHaveBeenCalled();
+    });
+
+    it('should update an album with new album name', async () => {
+      const dto = {
+        name: 'Hard Bop',
+        year: 1957,
+        artistId: null,
+      };
+
+      const newDto = {
+        name: 'Ritual',
+        year: 1957,
+        artistId: null,
+      };
+
+      const album = await albumController.createAlbum(dto);
+      const updateAlbum = await albumController.editAlbum(album.id, newDto);
+
+      expect(updateAlbum).toEqual({
+        id: album.id,
+        ...newDto,
+      });
     });
   });
 
-  it('should return an object with id', async () => {
-    const dto = {
-      name: 'Hard Bop',
-      year: 1957,
-      artistId: null,
-    };
+  describe('Method delete of the Album Controller', () => {
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        controllers: [AlbumController],
+        providers: [AlbumService],
+      })
+        .overrideProvider(AlbumService)
+        .useValue(mockAlbumService)
+        .compile();
 
-    const album = await albumController.createAlbum(dto);
+      albumController = module.get<AlbumController>(AlbumController);
+    });
 
-    expect(album).toHaveProperty('id');
-    expect(album).toHaveProperty('id', expect.any(String));
+    it('should delete an album', async () => {
+      const dto = {
+        name: 'Ritual',
+        year: 1957,
+        artistId: null,
+      };
+
+      const album = await albumController.editAlbum(uuidv4(), dto);
+
+      expect(album).toEqual({
+        id: expect.any(String),
+        ...dto,
+      });
+    });
+
+    it('should be called', async () => {
+      const dto = {
+        name: 'Ritual',
+        year: 1957,
+        artistId: null,
+      };
+
+      const album = await albumController.editAlbum(uuidv4(), dto);
+
+      expect(mockAlbumService.editAlbum).toHaveBeenCalled();
+    });
+
   });
 });
